63614f382e6cd9280c3d670d86538a817ed00558
Adil Hafeez <adil@katanemo.com>
2023-05-05 15:43:36 -0700

    remove -x

--- samples/ehr-service/infra/get_acmehealth_doctor_token.sh
+++ samples/ehr-service/infra/get_acmehealth_doctor_token.sh
@@ -1,2 +1,1 @@
-set -x
 . ../.subscriber_details && . ../.ehr_service_details && katutil login-with-password --service_id $EHR_SERVICE_ID --email $DOCTOR_EMAIL --password $DOCTOR_PASSWORD --account_id $SUBSCRIBER_ACCOUNT_ID | jq -r .token
