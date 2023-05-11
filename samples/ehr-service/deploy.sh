660e7cc146049c6d834bd608bcb45b893d95e19a
Adil Hafeez <adil@katanemo.com>
2023-05-11 15:08:31 -0700

    fix env var

--- samples/ehr-service/deploy.sh
+++ samples/ehr-service/deploy.sh
@@ -2,6 +2,8 @@ source common.sh
 source .ehr_admin_details
 set -e
 
+AUTH_ENDPOINT=https://auth.us-west-2.katanemo.dev
+
 if [[ -z "terraform" ]]; then
   cd tf_infra
   log terraform apply -var="client_key=$EHR_CLIENT_ID" -var="client_secret=$EHR_CLIENT_SECRET"
@@ -13,6 +15,6 @@ else
   log cdk bootstrap
   cdk bootstrap
 
-  log cdk deploy --parameters clientKey=$EHR_CLIENT_ID --parameters clientSecret=$EHR_CLIENT_SECRET
-  cdk deploy --parameters clientKey=$EHR_CLIENT_ID --parameters clientSecret=$EHR_CLIENT_SECRET
+  log cdk deploy --parameters clientKey=$EHR_CLIENT_ID --parameters clientSecret=$EHR_CLIENT_SECRET --parameters authEndpoint="$AUTH_ENDPOINT"
+  cdk deploy --parameters clientKey=$EHR_CLIENT_ID --parameters clientSecret=$EHR_CLIENT_SECRET --parameters authEndpoint="$AUTH_ENDPOINT"
 fi
