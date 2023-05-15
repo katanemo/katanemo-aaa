8eacc93cb801f1e7b148e553b7caff786651d671
Adil Hafeez <adil@katanemo.com>
2023-05-15 00:02:46 -0700

    deploy arc deploy using deploy.sh

--- samples/ehr-service/deploy.sh
+++ samples/ehr-service/deploy.sh
@@ -3,19 +3,19 @@ set -e
 
 source common.sh
 
+API_ENDPOINT=https://api.us-west-2.katanemo.dev
 KATUTIL=../../cli/bin/katutil
-AUTH_ENDPOINT=https://auth.us-west-2.katanemo.dev
 KATANEMO_ACCOUNT_EMAIL=$1
+EHR_SERVICE_ID=$2
 
-if [[ -z "$KATANEMO_ACCOUNT_EMAIL" ]]; then
-  echo "usage $0 <katanemo-account-email>"
+if [[ -z "$KATANEMO_ACCOUNT_EMAIL" ]] || \
+   [[ -z "$EHR_SERVICE_ID" ]]; then
+  echo "usage $0 <katanemo-account-email> <service-id>"
   exit 1
 fi
 
 echo Enter password for $KATANEMO_ACCOUNT_EMAIL
 read EHR_ADMIN_PASSWORD
-
-
 
 KATANEMO_SERVICE_ID=$($KATUTIL get-default-service | jq -r .serviceId)
 
@@ -38,18 +38,30 @@ EHR_CLIENT_SECRET=$(echo $EHR_CLIENT_KEY_DETAILS | jq -r .clientSecret)
 
 log created client key id $EHR_CLIENT_ID and client key secret xxxxx used for identifying API calls to Katanemo by EHR SaaS Service
 
+
+log setting up arc on ecs
+pushd ../../arc-ecs
+ADMIN_ACCOUNT_ID=$KATANEMO_ACCOUNT_ID SERVICE_ID=$EHR_SERVICE_ID  CLIENT_ID=$EHR_CLIENT_ID CLIENT_SECRET=$EHR_CLIENT_SECRET sh deploy.sh
+popd
+
 log npm install
 npm install
 
 log npm run build
 npm run build
 
+log getting the arc auth endpoint
+ARC_AUTH_ENDPOINT=$(aws cloudformation describe-stacks --query "Stacks[*].Outputs" --output json | jq '.[]' | jq '.[] | select(.OutputKey | test("KatanemoArcServiceURL")) | .OutputValue' -r)
+
+log arc endpoint address $ARC_AUTH_ENDPOINT
+
 log cdk bootstrap
 cdk bootstrap
 
-log cdk deploy --parameters clientKey=$EHR_CLIENT_ID --parameters clientSecret=xxxxx --parameters authEndpoint="$AUTH_ENDPOINT"
-cdk deploy --parameters clientKey=$EHR_CLIENT_ID --parameters clientSecret=$EHR_CLIENT_SECRET --parameters authEndpoint="$AUTH_ENDPOINT"
+log cdk deploy --parameters clientKey=$EHR_CLIENT_ID --parameters clientSecret=$EHR_CLIENT_SECRET --parameters apiEndpoint="$API_ENDPOINT" --parameters authEndpoint="$ARC_AUTH_ENDPOINT"
+cdk deploy --parameters clientKey=$EHR_CLIENT_ID --parameters clientSecret=$EHR_CLIENT_SECRET --parameters apiEndpoint="$API_ENDPOINT" --parameters authEndpoint="$ARC_AUTH_ENDPOINT"
 
 API_GATEWAY=$(aws cloudformation describe-stacks --query "Stacks[*].Outputs" --output json | jq '.[]' | jq '.[] | select(.OutputKey | test("patientRecordServiceEndpoint")) | .OutputValue' -r)
 
+log Katanemo ARC Endpoint: $API_GATEWAY
 log API Gateway configured with katanemo lambda authorizer $API_GATEWAY
